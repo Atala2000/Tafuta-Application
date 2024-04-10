b5049aeed3760a5d51ab50d1dd5b1f58d08ef772
Atala2000 <atalakidi@gmail.com>
2024-04-10 11:56:31 +0300

    Added a delete command to delete a specified object from the database Added a models array to improve errors

--- console.py
+++ console.py
@@ -1,15 +1,19 @@
 import cmd
 from models.users.database import DataStorage
 
+MODELS_ARRAY = ["Users", "Items", "Connected_Items"]
+
+
 class Tafuta(cmd.Cmd):
     """
     Class for the console
     """
+
     prompt = "(Tafuta) "
     intro = "Welcome to the Lost and Found Console"
 
     def __init__(self):
-        super().__init__(completekey='tab')
+        super().__init__(completekey="tab")
         self.data_storage = DataStorage()
 
     def do_quit(self, arg):
@@ -24,6 +28,13 @@ class Tafuta(cmd.Cmd):
         """
         return True
 
+    def do_test(self, arg):
+        """
+        Prints out the command
+        """
+        args = arg.split()
+        print(args)
+
     def emptyline(self):
         """
         Do nothing
@@ -35,7 +46,7 @@ class Tafuta(cmd.Cmd):
         Show all instances of a class
         """
         args = arg.split()
-        if args:
+        if args and args[0] in MODELS_ARRAY:
             try:
                 items = self.data_storage.all(args[0])
                 for item in items:
@@ -43,13 +54,41 @@ class Tafuta(cmd.Cmd):
             except Exception as e:
                 print(f"An error occurred: {e}")
         else:
-            print("Please provide a class name")
+            print("Allowed classnames are: ")
+            for model_class in MODELS_ARRAY:
+                print(model_class)
+            print()
+
+    def do_create(self, arg):
+        """
+        Initial setup to create the models on specified database
+        """
+        self.data_storage.create()
 
     def do_help(self, arg):
         """
         Show help for a command
         """
-        print("Help message")
+        super().do_help(arg)
+
+    def do_delete(self, arg):
+        """
+        Delete an instance of a class
+        """
+        args = arg.split()
+        if args and args[0] in MODELS_ARRAY:
+            try:
+                object_instance = self.data_storage.get(args[0], args[1])
+                self.data_storage.delete(object_instance)
+                print(f"Deleted {args[0]} with id {args[1]}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+        else:
+            print("Allowed classnames are: ")
+            for model_class in MODELS_ARRAY:
+                print(model_class)
+            print()
+
 
 if __name__ == "__main__":
     Tafuta().cmdloop()
