1d5f2c7aeeb57dfb54a3621b7142814b15044bb2
Atala2000 <atalakidi@gmail.com>
2024-04-11 06:05:06 +0300

    Reworked the layout, flask backend is now contained in the server directory Created a client directory that houses the front end portion Used vite to create a react app

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button'
+
+function App() {
+
+  return (
+    <>
+      <Button variant="contained">This is a test to emsure MUI works</Button>
+    </>
+  )
+}
+
+export default App
