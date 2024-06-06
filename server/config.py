9eaf2dde2236d19638a09a7f26c14402dd47b94d
Atala2000 <atalakidi@gmail.com>
2024-06-07 00:08:55 +0300

    chore: Update server configuration and ignore files

--- server/config.py
+++ server/config.py
@@ -1,5 +1,12 @@
 #config.py
+import uuid
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 #Configuration variables for the blueprint
 UPLOAD_FOLDER = "./uploads"
 USERS_ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+JWT_SECRET_KEY = uuid.uuid4()
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASS')}@localhost:33061/test"
