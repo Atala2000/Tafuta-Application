b9300bf44b35cccf5fbc9df42df410439558d186
Atala2000 <atalakidi@gmail.com>
2024-06-08 22:15:20 +0300

    chore: Refactor auth routes and add user signup functionality

--- server/app/auth/routes.py
+++ server/app/auth/routes.py
@@ -1,18 +1,20 @@
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from app.auth import bp as auth
+from app.models import models
 from flask import request, jsonify
 from app.models.models import Users
 from app.models.database import DataStorage
 import bcrypt
 
+data_storage = DataStorage()
 
 @auth.route("/login", methods=["POST"], strict_slashes=False)
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
 
-    data_storage = DataStorage()  # Instantiate DataStorage class
-    user = data_storage.filter(Users, email=email)[0] # Retrieve the user object
+    # Instantiate DataStorage class
+    user = data_storage.filter(Users, email=email)[0]  # Retrieve the user object
 
     if not user:
         return jsonify({"message": "User does not exist"}), 401
@@ -26,3 +28,38 @@ def login():
     else:
         return jsonify({"Error": "Authentication error, wrong password"}), 401
 
+
+@auth.route("/signup", methods=["POST"], strict_slashes=False)
+def signup():
+    """
+    Adds a new user to the database and logs them in
+    """
+    try:
+        email = request.json.get("email")
+        password = request.json.get("password")
+        first_name = request.json.get("first_name")
+        phone_no = request.json.get("phone_no")
+
+        password_bytes = password.encode("utf-8")
+        hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+
+        new_user = models.Users(
+            email=email,
+            password=hashed_password,
+            first_name=first_name,
+            phone_no=phone_no,
+        )
+        data_storage.add(new_user)
+
+        # Automatically log in the user after signup
+        access_token = create_access_token(identity=email)
+        return jsonify(message="User added", access_token=access_token), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@auth.route("/user", methods=["GET"], strict_slashes=False)
+@jwt_required()
+def get_user():
+    user =  get_jwt_identity()
+    return jsonify({"user": user})
