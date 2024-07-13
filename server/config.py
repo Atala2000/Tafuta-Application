18ae653d8ddeffb1acf313dc6f5e1972492b8c56
Atala2000 <atalakidi@gmail.com>
2024-07-13 15:47:23 +0300

    chore: Update UPLOAD_FOLDER path in config.py

--- server/config.py
+++ server/config.py
@@ -1,12 +1,12 @@
-#config.py
+# config.py
 import uuid
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
-#Configuration variables for the blueprint
-UPLOAD_FOLDER = "./uploads"
+# Configuration variables for the blueprint
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 USERS_ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 JWT_SECRET_KEY = str(uuid.uuid4())
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASS')}@localhost:33061/test"
