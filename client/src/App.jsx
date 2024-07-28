7ad0130ab67639b771a06a40171000fc38a7d5d9
Atala2000 <atalakidi@gmail.com>
2024-07-28 23:41:46 +0300

    chore: Update App.jsx to use DashBoard component for routing and navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import NavBar from "./components/Navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from "./components/Home";
-import Report from "./components/Report";
-import Items from "./components/Items";
+import { BrowserRouter } from "react-router-dom";
+import DashBoard from "./components/DashBoard";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Container> {/* Add top margin to create space */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/items" element={<Items />} />
-          <Route path="/report" element={<Report />} />
-        </Routes>
-      </Container>
+      <DashBoard />
     </BrowserRouter>
   )
 }
