c15e7799acf035f96c602a785d44d5ef7a56519c
Atala2000 <atalakidi@gmail.com>
2024-07-29 08:29:34 +0300

    chore: Update Login component to handle login functionality and navigation

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,55 +1,69 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const navigate = useNavigate();
+
     const handleLogin = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://localhost:5000/auth/login/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        });
-        const data = await response.json();
-        if (response.status === 200) {
-            localStorage.setItem('accessToken', data.access);
-            localStorage.setItem('refreshToken', data.refresh);
-        } else {
-            console.log(data);
+        try {
+            const response = await fetch('http://localhost:5000/auth/login/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Error:', errorData);
+                alert('Login failed: ' + errorData.message);
+                return;
+            }
+
+            const data = await response.json();
+            localStorage.setItem('accessToken', data.access_token);
+            localStorage.setItem('refreshToken', data.refresh_token);
+            console.log('Login successful:', data);
+            navigate('/home');
+        } catch (error) {
+            console.error('Fetch error:', error);
+            alert('An error occurred while logging in. Please try again later.');
         }
-    }
+    };
 
     return (
         <section id="login__form__container">
             <form id="login__form" onSubmit={handleLogin}>
                 <label htmlFor="username">
                     Username:
-                    <input 
-                        type="text" 
-                        id="username" 
-                        placeholder="Username" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)} 
+                    <input
+                        type="text"
+                        id="username"
+                        placeholder="Username"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </label>
                 <label htmlFor="password">
                     Password:
-                    <input 
-                        type="password" 
-                        id="password" 
-                        placeholder="Password" 
-                        value={password} 
-                        onChange={(e) => setPassword(e.target.value)} 
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </label>
                 <button type="submit">Login</button>
             </form>
         </section>
     );
-}
+};
 
 export default Login;
