510e4989ebea67cb22472af6b052f9261f24d614
Atala2000 <atalakidi@gmail.com>
2024-07-29 08:30:19 +0300

    chore: Update JWT token expiration times to 1 day

--- server/config.py
+++ server/config.py
@@ -9,7 +9,7 @@ dotenv.load_dotenv()
 # Configuration variables for the blueprint
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 USERS_ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
-JWT_SECRET_KEY = str(uuid.uuid4())
-JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=30)
-JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=30)
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=1)
+JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=1)
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASS')}@localhost:33061/test"
